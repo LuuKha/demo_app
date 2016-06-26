586cf96e536e66b5aa495244e239bff402b8d56b
Luu Kha <luu.tuan.kha@framgia.com>
2016-06-27 01:04:15 +0700

    finish Show User

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160624070641) do
+ActiveRecord::Schema.define(version: 20160624093519) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
     t.string   "password_digest"
-    t.string   "admin"
+    t.string   "admin",           default: "f"
     t.string   "boolean"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
+    t.string   "remember_digest"
   end
 
 end

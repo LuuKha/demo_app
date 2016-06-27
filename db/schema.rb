ba1ab4bf60128d227316fa2dd24709bb6cf0c1fa
Luu Kha <luu.tuan.kha@framgia.com>
2016-06-27 11:00:04 +0700

    make comment model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160627020629) do
+ActiveRecord::Schema.define(version: 20160627035055) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "entry_id"
+    t.integer  "user_id"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["entry_id"], name: "index_comments_on_entry_id"
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
   create_table "entries", force: :cascade do |t|
     t.text     "content"
